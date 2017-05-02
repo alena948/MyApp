006e660b3a6eacf9730f5c2817f111cb973aa15e
Алёна <alenabaranova948@gmail.com>
2017-05-02 23:58:45 +0300

    Reworked

--- src/client/GWTService.java
+++ src/client/GWTService.java
@@ -6,7 +6,7 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 /**
  * Created by Алёна on 24.03.2017.
  */
-@RemoteServiceRelativePath("sampleservice/gwtservice")
+@RemoteServiceRelativePath("sampleservice/gwtservice")//связывает service с путем по умолчанию относительно модуля базового URL.
 public interface GWTService extends RemoteService {
     public String myMethod(String s);
 }
--- src/client/Main.java
+++ src/client/Main.java
@@ -1,6 +1,7 @@
 package client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
@@ -70,6 +71,7 @@ public class Main implements EntryPoint {
                 Window.alert(event.getResults());
             }
         });
+        form.setAction(GWT.getModuleBaseURL() + "fileupload");
         RootPanel.get().add(form);
     }
 }
